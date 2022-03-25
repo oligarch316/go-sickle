3181f687e1c6d8dea4612a0bf0eaa48416a5039a
Oligarch E <>
2022-03-25 15:47:38 -0400

    Throw in a prettier json printout for now

--- pkg/command/session/config.go
+++ pkg/command/session/config.go
@@ -1,12 +1,14 @@
 package session
 
 import (
+	"encoding/json"
 	"fmt"
 	"os"
 
 	"github.com/oligarch316/go-sickle/pkg/command"
 	"github.com/oligarch316/go-sickle/pkg/config/data"
 	"github.com/spf13/cobra"
+	"go.uber.org/zap"
 )
 
 type configParams struct{ command.SessionParams }
@@ -17,7 +19,7 @@ func NewConfig(defaultConfig data.Config) *cobra.Command {
 	res := &cobra.Command{
 		Use:   "config",
 		Short: "Display loaded configuration",
-		Long:  "Display loaded configuration",
+		Long:  "TODO",
 		Run: func(_ *cobra.Command, _ []string) {
 			os.Exit(runConfig(params))
 		},
@@ -35,7 +37,12 @@ func runConfig(params configParams) int {
 		return status
 	}
 
-	// TODO: Fancier printing
-	fmt.Printf("%+v\n", bootstrap.Config)
+	b, err := json.MarshalIndent(bootstrap.Config, "", "  ")
+	if err != nil {
+		bootstrap.Logger.Error("failed to marshal config", zap.Error(err))
+		return 1
+	}
+
+	fmt.Println(string(b))
 	return 0
 }
